40f3df393d41f52caa749a9ec2e04835a101d9e2
cesslab <anwarruff@gmail.com>
2018-10-09 14:45:16 -0400

    Added template vars

--- maze_timing/pages.py
+++ maze_timing/pages.py
@@ -3,7 +3,13 @@ from .models import Constants
 
 
 class MazePage(Page):
-    pass
+    form_model = 'player'
+    form_fields = ['solved', 'solve_time_seconds']
+
+    def vars_for_template(self):
+        return {
+            'seconds_to_solve': self.session.config['seconds_to_solve_maze_1']
+        }
 
 
 page_sequence = [
