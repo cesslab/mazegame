27ab6af060ad00440fb8a4423402ce9db9b72d05
cesslab <anwarruff@gmail.com>
2018-10-10 18:39:00 -0400

    Added the instruction page

--- maze_timing/pages.py
+++ maze_timing/pages.py
@@ -2,16 +2,33 @@ from ._builtin import Page
 from .models import Constants
 
 
-class MazePage(Page):
-    form_model = 'player'
-    form_fields = ['solved', 'solve_time_seconds']
-
+class InstructionPage(Page):
     def vars_for_template(self):
+        maze_game = Constants.maze_game
         return {
-            'seconds_to_solve': self.session.config['seconds_to_solve_maze_1']
+            'num_mazes': maze_game.num_mazes(),
+            'minutes_to_solve': self.session.config['seconds_to_solve_maze']/60,
         }
 
 
-page_sequence = [
-    MazePage,
-]
+class MazePage(Page):
+    form_model = 'player'
+    form_fields = ['solved', 'solve_time_seconds', 'maze_id']
+
+    def vars_for_template(self):
+        maze = Constants.maze_game.maze(self.round_number)
+        return {
+            'seconds_to_solve': self.session.config['seconds_to_solve_maze'],
+            'maze_img': 'maze_timing/img/'+maze.name+'.png',
+            'maze_id': maze.name,
+            'start_x': maze.start_x,
+            'start_y': maze.start_y,
+            'end_x': maze.end_x,
+            'end_y': maze.end_y,
+            'round': self.round_number,
+        }
+
+
+maze_page_sequence = [MazePage]*Constants.num_rounds
+
+page_sequence = [InstructionPage] + maze_page_sequence
